60f080bab5f5af40c7581058db26a787cc09914b
Sergey Loguntsov <loguntsov@gmail.com>
2015-06-18 15:27:08 +0300

    Init.

---
+++ src/calypso_json.erl
@@ -0,0 +1,21 @@
+-module(calypso_json).
+-author("begemot").
+
+%% API
+-export([
+  encode/1, decode/1,
+  to_json/1, from_json/1
+]).
+
+encode(Term) ->
+  { ok, Opt } = application:get_env(calypso_json, encode_option),
+  jsx:encode(Term, Opt).
+
+decode(Binary) ->
+  jsx:decode(Binary).
+
+to_json(undefined) -> null;
+to_json(Any) -> Any.
+
+from_json(null) -> undefined;
+from_json(Any) -> Any.
